853d8f45ffee5d6278356ba055ac0f37ea3b5efd
Baurzhan <51234459+kiba013@users.noreply.github.com>
2023-04-25 05:24:09 +0000

    JavaIntroduction

---
+++ JavaIntroduction/Ifelse.java
@@ -0,0 +1,55 @@
+/* Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Sample Input 1
+24
+Sample Output 1
+Not Weird
+*/
+import java.io.*;
+
+
+public class Ifelse {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        String ans = "";
+
+        bufferedReader.close();
+        if(N % 2 == 1) {
+            ans = "Weird";
+        } else {
+            if(N >= 2 && N <= 5) {
+                ans = "Not Weird";
+            } else if(N >= 6 && N <= 20) {
+                ans = "Weird";
+            } else {
+                ans = "Not Weird";
+            }
+        }
+        System.out.println(ans);
+    }
+}
